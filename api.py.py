97587b8357bcf3821d195d1fc5705eb3d3825032
Saurav Kaushik <100580788+Sauravkaushik30@users.noreply.github.com>
2024-10-22 01:08:44 +0530

    Update api.py.py

--- api.py.py
+++ api.py.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -11,7 +5,6 @@ app = Flask(__name__)
 @app.route('/query', methods=['POST'])
 def query():
     user_query = request.json['query']
-    # Process the query like in the Streamlit app, return results
     ...
     return jsonify(results)
 
